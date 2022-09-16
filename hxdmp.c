9003d7dfb072b5d0db8f01d8cb62fb764cdab8ed
minneelyyyy <aprilkahny@gmail.com>
2022-09-15 21:59:33 -0400

    Update hxdmp.c

--- hxdmp.c
+++ hxdmp.c
@@ -8,7 +8,7 @@ int is_printable(char c) {
 void print_row(char row[16], int i, int current_line) {
 	int j;
 
-	printf("%06x ", current_line * 0x10);
+	printf("%08x | ", current_line * 0x10);
 
 	for (j = 0; j < 16; j++) {
 		if (j < i) printf("%02x", (unsigned char) row[j]);
@@ -44,7 +44,8 @@ int main(int argc, char* argv[]) {
 
 	i = 0;
 
-	printf("       00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
+	printf("           00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
+	printf("         +-------------------------------------------------+----------------+\n");
 
 	for (;;) {
 		c = fgetc(stream);
